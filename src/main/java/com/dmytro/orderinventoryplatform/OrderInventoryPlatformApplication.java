c0362e8cf87808fdd6143c8bdca996eb64d93087
Dmytro Dehtiarov <degtyarev.dmitry02@gmail.com>
2026-01-24 16:28:16 -0500

    Bootstrap Spring Boot skeleton with Postgres and Flyway

---
+++ src/main/java/com/dmytro/orderinventoryplatform/OrderInventoryPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dmytro.orderinventoryplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInventoryPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderInventoryPlatformApplication.class, args);
+	}
+
+}
