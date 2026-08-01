75d75121fd36585629b7f5c744c805eaace9318e
Dmytro Dehtiarov <degtyarev.dmitry02@gmail.com>
2026-08-01 11:01:26 -0400

    feat(catalog): add tests for Category and Product domain entities

---
+++ src/test/java/com/dmytro/orderinventoryplatform/catalog/domain/CategoryTest.java
@@ -0,0 +1,37 @@
+package com.dmytro.orderinventoryplatform.catalog.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CategoryTest {
+    @Test
+    void shouldCreateCategory() {
+        Category category = new Category("Books", "Book category", null);
+        Assertions.assertEquals("Books", category.getName());
+        Assertions.assertEquals("Book category", category.getDescription());
+        Assertions.assertNull(category.getParentCategory());
+    }
+
+    @Test
+    void shouldCreateSubCategory() {
+        Category category1 = new Category("Books", "Book category", null);
+        Category category2 = new Category("Fantasy", "Book category Fantasy", category1);
+        Assertions.assertEquals("Fantasy", category2.getName());
+        Assertions.assertEquals("Book category Fantasy", category2.getDescription());
+        Assertions.assertEquals(category1, category2.getParentCategory());
+    }
+
+    @Test
+    void shouldThrowWhenNameIsNull() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Category(null, "Book category", null);
+        });
+    }
+
+    @Test
+    void shouldThrowWhenNameIsBlank() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Category("   ", "Book category", null);
+        });
+    }
+}
